5bcd0276e56155de5c3236249a190e01879f0b71
jameslin3263 <kinokojiang@hotmail.com>
2019-11-11 15:09:30 +0800

    set react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import HomePage from './pages/homepage/HomePage'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
