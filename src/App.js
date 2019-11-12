2b51e2fa7876637d0efa91078413d964411ac206
jameslin3263 <kinokojiang@hotmail.com>
2019-11-12 16:13:33 +0800

    create collection item and preview components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 
-import HomePage from './pages/homepage/HomePage'
 import './App.css';
+
+import HomePage from './pages/homepage/HomePage'
+import ShopPage from './pages/shop/ShopPage'
+
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import './HomePage.scss'
 import Directory from '../../components/directory/Directory'
+import './HomePage.scss'
 
 const HomePage = () => {
     return (
