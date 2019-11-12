111416fb74d89c0c18225c235b12115cab113b39
jameslin3263 <kinokojiang@hotmail.com>
2019-11-12 16:27:28 +0800

    create header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/ShopPage'
+import Header from './components/header/Header'
 
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
